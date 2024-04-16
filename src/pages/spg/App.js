e3e5f1dacc74d6c95d34451b2e94b39ac77ef38c
noahrjh <jacksnu@mail.uc.edu>
2024-04-16 16:57:36 -0400

    Added App.js

--- src/pages/spg/App.js
+++ src/pages/spg/App.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Board from "./Board";
-import { updateURLParameter } from "./helpers"
+import { updateURLParameter } from "./Game"
 
 function App() {
   const [imgUrl, setImgUrl] = useState("")
 
+
+  // On page load, check if there is an image in the URL query params
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search)
     if (urlParams.has("img")) {
-      setImgUrl(urlParams.get("img"))
+      setImgUrl(urlParams.get("img")) //Game state is updated to the img we have the url for
     }
   }, [])
 
-  const handleImageChange = (e) => {
+  const handleImageChange = (e) => { 
     setImgUrl(e.target.value)
     window.history.replaceState("", "", updateURLParameter(window.location.href, "img", e.target.value))
   }
