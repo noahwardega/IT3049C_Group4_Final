c9ba351c5ff8e767465e8177bd1e19a80c0e5f24
noahrjh <jacksnu@mail.uc.edu>
2024-04-16 20:15:07 -0400

    changed Spg import

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RPSGame from './pages/rps/App';
 import Board from './pages/ttt/board';
-import App from './pages/spg/App';
+import Spg from './pages/spg/Spg';
 import './index.css';
-import App from './pages/rps/App'
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -18,7 +18,7 @@ root.render(
         <Route path="/IT3049C_Group4_Final/" element={<HomePage/>} />
         <Route path="/IT3049C_Group4_Final/rps" element={<RPSGame/>} />
         <Route path="/IT3049C_Group4_Final/ttt" element={<Board/>} />
-        <Route path="/IT3049C_Group4_Final/spg" element={<App />} />
+        <Route path="/IT3049C_Group4_Final/spg" element={<Spg/>} />
         {/* more routes */}
       </Routes>
     </BrowserRouter>
