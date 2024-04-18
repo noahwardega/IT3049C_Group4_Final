652f7a57aa81733ed014a939531155593e40db89
gracenordwick <92000737+gracenordwick@users.noreply.github.com>
2024-04-18 10:39:29 -0400

    main.jsx edited

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Board from './pages/ttt/board';
 import Spg from './pages/spg/Spg';
 import Word from './pages/word/App';
 import './index.css';
+import GuessingGame from './pages/gg/GuessingGame';
 
 
 const root = createRoot(document.getElementById('root'));
@@ -20,6 +21,8 @@ root.render(
         <Route path="/IT3049C_Group4_Final/rps" element={<RPSGame/>} />
         <Route path="/IT3049C_Group4_Final/ttt" element={<Board/>} />
         <Route path="/IT3049C_Group4_Final/spg" element={<Spg/>} />
+        <Route path="/IT3049C_Group4_Final/gg" element={<GuessingGame/>} />
+        
         {/* more routes */}
       </Routes>
     </BrowserRouter>
