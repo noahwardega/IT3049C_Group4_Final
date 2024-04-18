5eccf37a02295c25c01b48001c4bf34057e65bc7
gracenordwick <92000737+gracenordwick@users.noreply.github.com>
2024-04-18 11:55:12 -0400

    css changes

--- src/pages/gg/GuessingGame.jsx
+++ src/pages/gg/GuessingGame.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
 import Confetti from 'react-confetti';
-import './App.css';
+import './styles.css';
 
 function GuessingGame() {
   const [targetNumber, setTargetNumber] = useState(generateRandomNumber(1, 100));
   const [guess, setGuess] = useState('');
   const [feedback, setFeedback] = useState('');
   const [score, setScore] = useState(0);
-  const [showConfetti, setShowConfetti] = useState(false); // State to control confetti display
-
+  const [showConfetti, setShowConfetti] = useState(false); 
   function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function handleGuessChange(event) {
+  function handleTextChange(event) {
     setGuess(event.target.value);
   }
 
-  function handleGuessSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
     const parsedGuess = parseInt(guess, 10);
     if (isNaN(parsedGuess)) {
@@ -29,33 +28,33 @@ function GuessingGame() {
     } else {
       setFeedback(`Congratulations! ${targetNumber} is correct!`);
       setScore(score + 1);
-      setShowConfetti(true); // Activate confetti when the guess is correct
+      setShowConfetti(true); 
     }
   }
 
-  function handleReset() {
+  function handleGameReset() {
     setTargetNumber(generateRandomNumber(1, 100));
     setGuess('');
     setFeedback('');
-    setShowConfetti(false); // Reset confetti display
+    setShowConfetti(false); 
   }
 
   return (
     <div className="number-guessing-game">
-      {showConfetti && <Confetti />} {/* Render confetti if showConfetti is true */}
+      {showConfetti && <Confetti />} 
       <h1>Number Guessing Game</h1>
       <p>Score: {score}</p>
-      <form onSubmit={handleGuessSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="number"
           value={guess}
-          onChange={handleGuessChange}
-          placeholder="Enter your guess"
+          onChange={handleTextChange}
+          placeholder="Type your guess here..."
         />
         <button type="submit">Guess</button>
       </form>
       {feedback && <p>{feedback}</p>}
-      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleGameReset}>Reset Game</button>
     </div>
   );
 }
